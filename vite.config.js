d7a6e38a2b2e7826cdb670a75c2f4f5938663fe4
Anas Zahra <pib.anas.zahra@htwsaar.de>
2023-06-06 19:31:09 +0200

    solved the problem in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -16,5 +15,10 @@ export default defineConfig({
         additionalData: `@import '@/assets/base.css';`
       }
     }
-  }
-})
+  },
+  define: {
+    // By default, Vite doesn't include shims for NodeJS,
+    // necessary for segment analytics lib to work
+    global: {},
+  },
+});
