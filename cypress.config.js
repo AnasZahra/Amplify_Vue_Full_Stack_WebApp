2b1ad855931a4808f83dfa3724bb2a0c40074c1e
AliBahloul91 <85230669+AliBahloul91@users.noreply.github.com>
2023-07-03 16:01:06 +0200

    mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
-    setupNodeEvents() {
+    setupNodeEvents(on) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
