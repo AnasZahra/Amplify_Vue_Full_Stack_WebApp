c915364d1cd82c80eadcc8f65f8f9c4a5f943dd6
Anas Zahra <pib.anas.zahra@htwsaar.de>
2023-09-06 00:18:14 +0200

    vue-table from PRIMEVUE

--- src/Workoutpage.vue
+++ src/Workoutpage.vue
@@ -1,115 +1,105 @@
 <template>
+
+<section class="features" id="features">
+          <div>
+
+            <a href="/profile" class="main-btn-up">Create an account</a>
+  
+          </div>
+  </section>
+
   <div>
-    <section class="features" id="features">
 
-        <h2 class="titel">Choose your favourite Workout</h2>
-        <button @click="createGymPlan">Generate Gym Plan</button>
-
-      <div class="content">
-
-        <div class="card">
-          <div class="icon">
-            <i class="fa-solid fa-dumbbell"></i>
-          </div>
-          <div class="info">
-            <h3>Personalized Workouts</h3>
-            <p>Create a customized workout plan in minutes</p>
-          </div>
-        </div>
-
-        <div class="card">
-          <div class="icon">
-            <i class="fa-solid fa-brain"></i>
-          </div>
-          <div class="info">
-            <h3>Find Your Training Style</h3>
-            <p>Discover the perfect training method for you</p>
-          </div>
-        </div>
-
-        <div class="card">
-          <div class="icon">
-            <i class="fa-solid fa-heart-pulse"></i>
-          </div>
-          <div class="info">
-            <h3>Track Your Progress</h3>
-            <p>Stay on top of your fitness journey and track all your workouts</p>
-          </div>
-        </div>
-
-                <div class="card">
-          <div class="icon">
-            <i class="fa-solid fa-dumbbell"></i>
-          </div>
-          <div class="info">
-            <h3>Personalized Workouts</h3>
-            <p>Create a customized workout plan in minutes</p>
-          </div>
-        </div>
-
-        <div class="card">
-          <div class="icon">
-            <i class="fa-solid fa-brain"></i>
-          </div>
-          <div class="info">
-            <h3>Find Your Training Style</h3>
-            <p>Discover the perfect training method for you</p>
-          </div>
-        </div>
-
-        <div class="card">
-          <div class="icon">
-            <i class="fa-solid fa-heart-pulse"></i>
-          </div>
-          <div class="info">
-            <h3>Track Your Progress</h3>
-            <p>Stay on top of your fitness journey and track all your workouts</p>
-          </div>
-        </div>
-
-                <div class="card">
-          <div class="icon">
-            <i class="fa-solid fa-heart-pulse"></i>
-          </div>
-          <div class="info">
-            <h3>Track Your Progress</h3>
-            <p>Stay on top of your fitness journey and track all your workouts</p>
-          </div>
-        </div>
-
-      </div>
-    </section>
-
-  </div>
+  <DataTable v-model:editingRows="editingRows" :value="products" editMode="row" dataKey="id"
+        @row-edit-save="onRowEditSave" tableClass="editable-cells-table" tableStyle="min-width: 50rem">
+    <Column field="Day" header="Day" style="width: 20%">
+        <template #editor="{ data, field }">
+            <InputText v-model="data[field]" />
+        </template>
+    </Column>
+    <Column field="Workout part" header="Workout part" style="width: 20%">
+        <template #editor="{ data, field }">
+            <InputText v-model="data[field]" />
+        </template>
+    </Column>
+    <Column field="Time of the day" header="Time of the day" style="width: 20%">
+        <template #editor="{ data, field }">
+            <Dropdown v-model="data[field]" :options="statuses" optionLabel="label" optionValue="value" placeholder="Select a Status">
+                <template #option="slotProps">
+                    <Tag :value="slotProps.option.value" :severity="getStatusLabel(slotProps.option.value)" />
+                </template>
+            </Dropdown>
+        </template>
+        <template #body="slotProps">
+            <Tag :value="slotProps.data.inventoryStatus" :severity="getStatusLabel(slotProps.data.inventoryStatus)" />
+        </template>
+    </Column>
+    <Column field="price" header="Price" style="width: 20%">
+        <template #body="{ data, field }">
+            {{ formatCurrency(data[field]) }}
+        </template>
+        <template #editor="{ data, field }">
+            <InputNumber v-model="data[field]" mode="currency" currency="USD" locale="en-US" />
+        </template>
+    </Column>
+    <Column :rowEditor="true" style="width: 10%; min-width: 8rem" bodyStyle="text-align:center"></Column>
+  </DataTable>
+</div>
 </template>
 
 <script>
-import Footer from '@/components/Footer.vue'
 
+import { ProductService } from '@/service/ProductService';
 export default {
-  name: 'Workoutpage',
-  components: {
-    Footer
-  },
-  methods: {
-    createGymPlan () {
-      // Example logic for generating a gym plan
-      const exercises = ['Push-ups', 'Squats', 'Bench Press', 'Plank', 'Deadlift']
-      const plan = []
+    data() {
+        return {
+            products: null,
+            editingRows: [],
+            statuses: [
+                { label: 'Morning', value: 'Morning' },
+                { label: 'Mid-day', value: 'Mid-day' },
+                { label: 'Evening', value: 'Evening' }
+            ]
+        };
+    },
+    mounted() {
+        ProductService.getWorkoutPlanMini().then((data) => (this.products = data));
+    },
+    methods: {
+        onRowEditSave(event) {
+            let { newData, index } = event;
 
-      // Generate a plan with 3 random exercises
-      for (let i = 0; i < 3; i++) {
-        const randomIndex = Math.floor(Math.random() * exercises.length)
-        const exercise = exercises[randomIndex]
-        plan.push(exercise)
-      }
+            this.products[index] = newData;
+        },
+        getStatusLabel(status) {
+            switch (status) {
+                case 'INSTOCK':
+                    return 'success';
 
-      console.log(plan)
+                case 'LOWSTOCK':
+                    return 'warning';
+
+                case 'OUTOFSTOCK':
+                    return 'danger';
+
+                default:
+                    return null;
+            }
+        },
+        formatCurrency(value) {
+            return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value);
+        }
     }
-  }
-}
+};
+
 </script>
 
-<style src="./assets/base.css">
 
+
+
+<style lang="css" scoped>
+::v-deep(.editable-cells-table td.p-cell-editing) {
+    padding-top: 0.6rem;
+    padding-bottom: 0.6rem;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,14 +3,26 @@ import App from './App.vue'
 import router from './router'
 import awsConfig from './aws-exports'
 import { Auth } from 'aws-amplify'
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import PrimeVue from 'primevue/config';
 
 import '@aws-amplify/ui-vue/styles.css'
 import './assets/base.css'
 
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   // optional
+import Row from 'primevue/row';                   // optional
+
 Auth.configure(awsConfig)
 
-const app = createApp(App)
 
+const app = createApp(App)
+app.use(PrimeVue);
+app.component('DataTable', DataTable);
+app.component('Column', Column);
+app.component('ColumnGroup', ColumnGroup);
+app.component('Row', Row);
 app.use(router)
 
 app.mount('#app')
