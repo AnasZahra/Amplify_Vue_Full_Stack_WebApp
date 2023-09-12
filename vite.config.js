4fa3afaba6ba5959858ab336adaa02f36a34deb1
mhabeel <121513154+mhabeel@users.noreply.github.com>
2023-09-12 11:23:40 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,6 @@ export default defineConfig({
   define: {
     // By default, Vite doesn't include shims for NodeJS,
     // necessary for segment analytics lib to work
-    global: {}
+    // global: {}
   }
 })
